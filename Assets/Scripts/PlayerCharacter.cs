00f1527ed53f3043296e3e410b58ca0db1798922
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2021-08-15 15:03:30 -0300

    Update PlayerCharacter.cs

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -5,53 +5,52 @@ using UnityEngine;
 public class PlayerCharacter : MonoBehaviour, ISlider, IJumper
 {
     [SerializeField] private float jumpForce = 9.5f;
-    
+
     private Collider col;
     private Rigidbody rb;
     private SwipeDetection inputDetection;
-    private float distanceToGround;
     
+    private float distanceToGround;
+
     private bool isGrounded => Physics.Raycast(transform.position, -Vector3.up, distanceToGround);
+
+    #region Unity Events
 
     private void Awake()
     {
         inputDetection = SwipeDetection.Instance;
     }
-    
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         col = GetComponent<Collider>();
         distanceToGround = col.bounds.extents.y;
     }
+
     private void OnEnable()
     {
         inputDetection.OnSwipeUp += Jump;
-
     }
 
     private void OnDisable()
     {
         inputDetection.OnSwipeUp -= Jump;
-        
     }
+
+    #endregion
+
+    #region Slide Interface
+
     public void Slide()
     {
         throw new System.NotImplementedException();
     }
 
+    #endregion
 
+    #region Jump Interface
 
-    public bool IsGrounded(Collider col)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public float DisTanceToGround(Collider col)
-    {
-        return col.bounds.extents.y;
-    }
-    
     public void Jump()
     {
         if (isGrounded)
@@ -59,4 +58,7 @@ public class PlayerCharacter : MonoBehaviour, ISlider, IJumper
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
+
+    #endregion
+    
 }
