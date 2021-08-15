8f998c84f64ba104717b943d425f5fa8ede7654e
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2021-08-15 14:24:02 -0300

    Added Jump Events

--- Assets/Scripts/Jumper.cs
+++ Assets/Scripts/Jumper.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Jumper : MonoBehaviour
@@ -8,22 +9,38 @@ public class Jumper : MonoBehaviour
     private Collider col;
     private Rigidbody rb;
     private float distanceToGround;
+
+    private SwipeDetection inputDetection;
+    private void Awake()
+    {
+        inputDetection = SwipeDetection.Instance;
+    }
+
+    private void OnEnable()
+    {
+        inputDetection.OnSwipeUp += Jump;
+
+    }
+
+    private void OnDisable()
+    {
+        inputDetection.OnSwipeUp -= Jump;
+        
+    }
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         col = GetComponent<Collider>();
         distanceToGround = col.bounds.extents.y;
     }
-    private void Update()
+    private bool isGrounded => Physics.Raycast(transform.position, -Vector3.up, distanceToGround);
+    
+    private void Jump()
     {
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
-    }
-    
-    private bool IsGrounded()
-    {
-        return Physics.Raycast(transform.position, -Vector3.up, distanceToGround);
     }
 }
