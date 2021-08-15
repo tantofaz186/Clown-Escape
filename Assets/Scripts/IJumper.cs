f7d8421caf5347095e858f41ed36a01a96f10488
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2021-08-15 14:53:26 -0300

    Removed Jumper Class for IJumper Interface

--- Assets/Scripts/IJumper.cs
+++ Assets/Scripts/IJumper.cs
@@ -3,7 +3,5 @@ using UnityEngine;
 
 public interface IJumper
 {
-    bool IsGrounded(Collider col);
-    float DisTanceToGround(Collider col);
     void Jump();
 }
--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -2,15 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCharacter : MonoBehaviour, ISlider
+public class PlayerCharacter : MonoBehaviour, ISlider, IJumper
 {
+    [SerializeField] private float jumpForce = 9.5f;
     
+    private Collider col;
+    private Rigidbody rb;
     private SwipeDetection inputDetection;
+    private float distanceToGround;
+    
+    private bool isGrounded => Physics.Raycast(transform.position, -Vector3.up, distanceToGround);
+
     private void Awake()
     {
         inputDetection = SwipeDetection.Instance;
     }
-
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+        distanceToGround = col.bounds.extents.y;
+    }
     private void OnEnable()
     {
         inputDetection.OnSwipeUp += Jump;
@@ -22,20 +35,28 @@ public class PlayerCharacter : MonoBehaviour, ISlider
         inputDetection.OnSwipeUp -= Jump;
         
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void Slide()
     {
         throw new System.NotImplementedException();
+    }
+
+
+
+    public bool IsGrounded(Collider col)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public float DisTanceToGround(Collider col)
+    {
+        return col.bounds.extents.y;
+    }
+    
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
