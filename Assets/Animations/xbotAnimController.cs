60995eb68b29c22720c48098eaf5deb53d87ff12
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2021-08-30 15:02:12 -0300

    Update xbotAnimController.cs

--- Assets/Animations/xbotAnimController.cs
+++ Assets/Animations/xbotAnimController.cs
@@ -1,13 +1,56 @@
+using System;
 using UnityEngine;
 
 namespace Animations
 {
     public class xbotAnimController : MonoBehaviour
     {
+        private Animator m_Animator;
+        private SwipeDetection inputDetection;
+
+        private static readonly int s_Slide = Animator.StringToHash("Slide");
+        private static readonly int s_Roll = Animator.StringToHash("Roll");
+
         // Start is called before the first frame update
+        private void Awake()
+        {
+            inputDetection = SwipeDetection.Instance;
+            
+        }
+
+        public void StartSlide()
+        {
+            m_Animator.applyRootMotion = true;
+        }
+        public void EndSlide()
+        {
+            m_Animator.applyRootMotion = false;
+        }
+        void SetRollTrigger()
+        {
+            m_Animator.SetTrigger(s_Roll);
+            
+        }
+        void SetSlideTrigger()
+        {
+            m_Animator.SetTrigger(s_Slide);
+            
+        }
+        private void OnEnable()
+        {
+            inputDetection.OnSwipeLeft += SetRollTrigger;
+            inputDetection.OnSwipeDown += SetSlideTrigger;
+            
+        }
+
+        private void OnDisable()
+        {
+            inputDetection.OnSwipeLeft -= SetRollTrigger;
+            inputDetection.OnSwipeDown -= SetSlideTrigger;
+        }
         void Start()
         {
-        
+            m_Animator = GetComponent<Animator>();
         }
 
         // Update is called once per frame
