e22a956022690ce9c27c58bb06d2d77e249dffab
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2021-10-06 15:29:42 -0300

    Added finish line and game win condition

--- Assets/Scripts/Controllers/GameEventHandler.cs
+++ Assets/Scripts/Controllers/GameEventHandler.cs
@@ -3,23 +3,28 @@ using UnityEngine;
 
 public class GameEventHandler : MonoBehaviour
 {
+    [SerializeField]private FinishLine LevelFinishLine;
     #region Event Subscription
 
     private void OnEnable()
     {
         Enemy.CollidedWithCharacter += GameOver;
-
+        LevelFinishLine.OnFinish += GameWin;
     }
 
     private void OnDisable()
     {
         Enemy.CollidedWithCharacter -= GameOver;
-
+        LevelFinishLine.OnFinish -= GameWin;
     }
 
     #endregion
 
     private void GameOver()
+    {
+        throw new NotImplementedException();
+    }
+    private void GameWin()
     {
         throw new NotImplementedException();
     }
