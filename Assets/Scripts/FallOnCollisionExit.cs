d511f525f6f3d404684395dc7b32a2359eb3aeff
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2021-11-21 13:13:37 -0300

    Update FallOnCollisionExit.cs

--- Assets/Scripts/FallOnCollisionExit.cs
+++ Assets/Scripts/FallOnCollisionExit.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class FallOnCollisionExit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionExit(Collision other)
     {
-        
+        Fall();
+    }
+
+    private void Fall()
+    {
+        rb.useGravity = true;
+        rb.isKinematic = false;
     }
 }
