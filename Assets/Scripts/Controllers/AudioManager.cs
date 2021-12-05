21fe5955ff7e6c3235f77d8c36d5d97e1878aa61
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2021-12-05 19:20:17 -0300

    .

--- Assets/Scripts/Controllers/AudioManager.cs
+++ Assets/Scripts/Controllers/AudioManager.cs
@@ -6,17 +6,13 @@ namespace Controllers
     public class AudioManager : Singleton<AudioManager>
     {
         public AudioSource audioSource;
-        public AudioSource musicSource;
 
         private void Awake()
         {
             if (audioSource == null)
                 audioSource = GetComponent<AudioSource>();
-            if (PlayerPrefs.HasKey("Volume"))
-            {
+            if(PlayerPrefs.HasKey("Volume"))
                 audioSource.volume = PlayerPrefs.GetFloat("Volume");
-                musicSource.volume = PlayerPrefs.GetFloat("Volume");
-            }
         }
     }
 }
