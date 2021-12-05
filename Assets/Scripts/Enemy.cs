6d0b7fd813ca9d5d1d06c96206e6757a5ecfaace
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2021-12-05 17:20:45 -0300

    .

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,6 @@ public class Enemy : GameOverOnCollision
 
     private void FixedUpdate()
     {
-        transform.Translate(Vector3.right * (Time.deltaTime * speed));
+        transform.Translate(Vector3.back * (Time.deltaTime * speed));
     }
 }
